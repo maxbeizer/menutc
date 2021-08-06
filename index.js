d6bcbffae77c30238f8b4c7e2a9188d53a9cb00d
Max Beizer <max.beizer@gmail.com>
2021-08-06 16:34:10 -0500

    Run prettier on the file

--- index.js
+++ index.js
@@ -1,59 +1,56 @@
-const { app, Tray, nativeImage } = require('electron');
-const { menubar } = require('menubar');
-let MenUTC = undefined
-let tray = undefined
+const { app, Tray, nativeImage } = require("electron");
+const { menubar } = require("menubar");
+let MenUTC = undefined;
+let tray = undefined;
 const MENU_BAR_OPTIONS = {
   showDockIcon: false,
   index: false,
-  browserWindow: { height: 0, width: 0 }
-}
+  browserWindow: { height: 0, width: 0 },
+};
 
-app.on('ready', () => {
+app.on("ready", () => {
   // Don't show the app in the doc
-  app.dock.hide()
-  tray = createTray()
-  MenUTC = createMb({tray})
+  app.dock.hide();
+  tray = createTray();
+  MenUTC = createMb({ tray });
 
-	MenUTC.on('ready', () => {
+  MenUTC.on("ready", () => {
     setInterval(() => {
-      checkTime()
+      checkTime();
     }, 1000); // every second
   });
 
   // debug only
-  // MenUTC.on('after-create-window', () => {
-  //   mb.window.openDevTools()
-  // })
+  // MenUTC.on("after-create-window", () => {
+  //   MenUTC.window.openDevTools();
+  // });
 });
 
 const checkTime = () => {
-  setTrayTitle({tray})
-}
+  setTrayTitle({ tray });
+};
 
 const createTray = () => {
   tray = new Tray(nativeImage.createEmpty());
-  setTrayTitle({tray})
-  return tray
-}
+  setTrayTitle({ tray });
+  return tray;
+};
 
-const createMb = ({tray}) => {
-	return menubar({
+const createMb = ({ tray }) => {
+  return menubar({
     tray,
-    ...MENU_BAR_OPTIONS
-	});
-}
+    ...MENU_BAR_OPTIONS,
+  });
+};
 
-const setTrayTitle = ({tray}) => tray.setTitle(buildTime())
+const setTrayTitle = ({ tray }) => tray.setTitle(buildTime());
 
 const buildTime = () => {
-  const time = [
-    new Date().getUTCHours(),
-    new Date().getUTCMinutes()
-  ]
-  .map(formatNumber)
-  .join(':')
+  const time = [new Date().getUTCHours(), new Date().getUTCMinutes()]
+    .map(formatNumber)
+    .join(":");
 
-  return `${time} UTC`
-}
+  return `${time} UTC`;
+};
 
-const formatNumber = number => `0${number}`.slice(-2)
+const formatNumber = (number) => `0${number}`.slice(-2);
